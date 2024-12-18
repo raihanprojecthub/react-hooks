43dcc2aa96a97ed7a1a416473d9edc95ad00bff1
raihanprojecthub <raihanramadhanp09@gmail.com>
2024-12-18 20:59:53 +0700

    Dynamic Values

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  const title = "Welcome to My App";
+  const age = 20;
+  const link = "https://raihan-ramadhan.my.id/";
+
+  return (
+    <div>
+      <h1>{ title }</h1>
+      <h1>{ age * 2 }</h1>
+      <a href={link}>Go To My Website</a>
+    </div>
+  )
+}
+
+export default App
