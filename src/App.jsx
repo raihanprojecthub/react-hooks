bf04e537e3ca6d12695e4f9e29d43231f72599c7
raihanprojecthub <raihanramadhanp09@gmail.com>
2024-12-18 21:18:23 +0700

    Click Events

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ function App() {
   const age = 20;
   const link = "https://raihan-ramadhan.my.id/";
 
+  const clickMe = (name) => {
+    console.log('Hello: '+ name);
+  }
+
   return (
     <div>
       <Header />
       <h1>{ title }</h1>
       <h1>{ age * 2 }</h1>
       <a href={link}>Go To My Website</a>
+      <button onClick={ () => clickMe('Raihan') }>Click Me</button>
     </div>
   )
 }
