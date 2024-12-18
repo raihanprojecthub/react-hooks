c54b4c1b0c9592702b4ee454cc888691213cd8ba
raihanprojecthub <raihanramadhanp09@gmail.com>
2024-12-18 21:35:14 +0700

    Looping Lists

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,32 @@ import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
-  const [title, setTitle] = useState("Welcome to My App");
-  const [age, setAge] = useState(20);
+  // const [title, setTitle] = useState("Welcome to My App");
+  // const [age, setAge] = useState(20);
+  const [products, setProducts] = useState([
+    { id: 1, title: 'Product 1', price: 899 },
+    { id: 2, title: 'Product 2', price: 769 },
+    { id: 3, title: 'Product 3', price: 989 },
+    { id: 4, title: 'Product 4', price: 871 },
+    { id: 5, title: 'Product 5', price: 459 },
+  ]);
 
-  const changeTitle = () => {
-    setTitle('Title Changed');
-    setAge(40);
-  }
+  // const changeTitle = () => {
+  //   setTitle('Title Changed');
+  //   setAge(40);
+  // }
 
   return (
     <div>
       <Header />
-      <h1>{ title }</h1>
+      {/* <h1>{ title }</h1>
       <h1>Age: { age }</h1>
-      <button onClick={ changeTitle }>Change Title</button>
+      <button onClick={ changeTitle }>Change Title</button> */}
+      <ul>
+        { products.map((product) => (
+          <li key={product.id}>{ product.title } - { product.price } </li>
+        )) }
+      </ul>
     </div>
   )
 }
